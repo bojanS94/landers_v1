8ae317ba8b8163a59e367bdd948967430ae48333
bojanS94 <62501546+bojanS94@users.noreply.github.com>
2020-12-30 18:02:09 +0100

    Latest update

--- js/app.js
+++ js/app.js
@@ -2,19 +2,14 @@ $('#btn, plan_btn').click(function () {
     $('#form').fadeIn('fast');
     $('body').css('overflow', 'hidden');
 });
-
 $('.close').click(function () {
     $('#form').fadeOut('fast');
     $('body').css('overflow', 'auto');
 });
-
 $('.burger').click(function () {
-
     $('.links').slideToggle('fast');
     $('#wrapper').toggleClass('margin');
-
 });
-
 $('#saveForm').click(function (e) {
     e.preventDefault();
 });
